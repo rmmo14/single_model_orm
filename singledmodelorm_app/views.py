3f63cfbc4925c3319b17eb0d8a61524e1505c7d3
Rene Montoya <rmontoya.we@gmail.com>
2020-11-19 11:11:20 -0800

    printing users to test

--- singledmodelorm_app/views.py
+++ singledmodelorm_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def adduser(request):
-    return None
+    context = {
+        'all_users': User.objects.all()
+    }
+    print(context)
+    return render(request, "index.html", context)
