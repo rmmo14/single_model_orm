4eae4b9cf377d8697ca20e4a3f44dec5ca4af139
Rene Montoya <rmontoya.we@gmail.com>
2020-11-19 21:12:00 -0800

    finishing users with templates minus CSS

--- singledmodelorm_app/views.py
+++ singledmodelorm_app/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
-
-def adduser(request):
     context = {
         'all_users': User.objects.all()
     }
-    print(context)
     return render(request, "index.html", context)
+
+def adduser(request):
+    User.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email = request.POST['email'],
+        age = request.POST['age']
+    )
+    return redirect('/')
